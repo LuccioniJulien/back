4cdd096a96668b5d7341f7c912d45079e5c87931
Julien <julien.luccioni@protonmail.com>
2018-03-09 17:14:56 +0100

    a

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ app.use(cookieParser())
 app.use('/', index)
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message
-//   res.locals.error = req.app.get('env') === 'development' ? err : {}
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-//   // render the error page
-//   res.status(err.status || 500)
-//   res.json({ error: 'sorry' })
-// })
+  // render the error page
+  res.status(err.status || 500)
+  res.json({ error: 'sorry' })
+})
 
 module.exports = app
