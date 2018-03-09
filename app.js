36792412c3a154e0a06e685cc42b94fe7342d31e
Julien <julien.luccioni@protonmail.com>
2018-03-09 11:53:01 +0100

    corection

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use('/', index)
+
 //app.use('/api/v1/users', users)
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.json({error='sorry'})
+})
 
 module.exports = app
